1710af8e9c494e244b62f0b84a95e11bdbf5682a
Ryan Weaver <ryan@thatsquality.com>
2010-06-27 18:41:06 -0500

    Rebuilding the test database, removing some of the test cleanup function, removing old model tables.

--- test/bootstrap/cleanup.php
+++ test/bootstrap/cleanup.php
@@ -7,17 +7,8 @@ function sympal_cleanup()
   rmdir(dirname(__FILE__).'/../fixtures/project/cache');
   rmdir(dirname(__FILE__).'/../fixtures/project/log');
   
-  $currentDir = getcwd();
-  chdir(dirname(__FILE__).'/../fixtures/project');
-  exec('git checkout *');
-  chdir($currentDir);
-  
   mkdir(dirname(__FILE__).'/../fixtures/project/cache', 0777, true);
   mkdir(dirname(__FILE__).'/../fixtures/project/log', 0777, true);
-
-  @unlink(dirname(__FILE__) . '/../fixtures/project/data/test.sqlite');
-  
-
 }
 
 copy(dirname(__FILE__).'/../fixtures/project/data/fresh_test_db.sqlite', dirname(__FILE__).'/../fixtures/project/data/test.sqlite');
