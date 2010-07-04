5a56ac9e94e840ccd28a4a69ab5e76e01ea97102
Ryan Weaver <ryan@thatsquality.com>
2010-07-04 12:35:10 -0500

    [config] Mostly moving methods around the sfSympalConfig class and adding phpDoc. There weren't any real changes.

--- lib/config/sfSympalConfig.class.php
+++ lib/config/sfSympalConfig.class.php
@@ -1,58 +1,33 @@
 <?php
 
 /**
- * Extension of sfConfig to allow us to add some shortcuts and build on top of the 
- * normal sfConfig
+ * Extension of sfConfig to allow easy access to configuration values
+ * under the "sympal_config" key as well as other helper methods.
  *
- * @package sfSympalPlugin
- * @author Jonathan H. Wage <jonwage@gmail.com>
+ * @package     sfSympalPlugin
+ * @subpackage  config
+ * @author      Jonathan H. Wage <jonwage@gmail.com>
+ * @author      Ryan Weaver <ryan.weaver@iostudio.com>
  */
 class sfSympalConfig extends sfConfig
 {
   /**
-   * Returns the "name" of the current site
+   * Return a configuration value under the app.yml "sympal_config" key
    *
-   * @throws sfException
-   * @return string
-   */
-  public function getCurrentSiteName()
-  {
-    if (!$site = sfConfig::get('sf_sympal_site', sfConfig::get('sf_app')))
-    {
-      throw new sfException('No current site defined. If you receive this error in a task, be sure to initialize an application configuration.');
-    }
-
-    return $site;
-  }
-
-  /**
-   * Check whether a Doctrine query result cache key should use result cache or not
+   * @example
+   * all:
+   *   sympal_config:
+   *     foo:        bar
+   *     deeper:
+   *       foo_deep: bar_deep
    *
-   * @param string $key 
-   * @return boolean
-   */
-  public static function shouldUseResultCache($key)
-  {
-    if (isset(self::$config['app_sympal_config_orm_cache']['queries'][$key]['enabled'])
-      && self::$config['app_sympal_config_orm_cache']['queries'][$key]['enabled']
-      && isset(self::$config['app_sympal_config_orm_cache']['result'])
-      && self::$config['app_sympal_config_orm_cache']['result']
-    )
-    {
-      return isset(self::$config['app_sympal_config_orm_cache']['queries'][$key]['lifetime']) ? self::$config['app_sympal_config_orm_cache']['queries'][$key]['lifetime'] : self::$config['app_sympal_config_orm_cache']['lifetime'];
-    }
-    else
-    {
-      return false;
-    }
-  }
-
-  /**
-   * Get a setting value
+   * sfSympalConfig::get('foo')  // returns bar
+   * sfSympalConfig::get('deeper', 'foo_deep') // return bar_deep
    *
-   * @param string $group 
-   * @param string $name 
-   * @param string $default 
+   * @param string $group   The top-level config key after "sympal_config"
+   * @param string $name    The next-level config key after $group (or null)
+   * @param mixed  $default The default value to return if the config doesn't exist
+   * 
    * @return mixed $value
    */
   public static function get($group, $name = null, $default = null)
@@ -69,35 +44,9 @@ class sfSympalConfig extends sfConfig
   }
 
   /**
-   * Helper to return very deep configuration values
-   * 
-   * all:
-   *   sympal_config:
-   *     group:
-   *       subGroup:
-   *         name:      value
-   * 
-   * @param string $group The group name for the config
-   * @param string $subGroup The sub group name for the config
-   * @param string $name The name of the config key
-   * @param mixed $default The default value to return if the config doesn't exist
-   */
-  public static function getDeep($group, $subgroup, $name, $default = null)
-  {
-    $data = sfSympalConfig::get($group, $subgroup);
-    
-    if (!$data || !is_array($data))
-    {
-      return $default;
-    }
-    
-    return isset($data[$name]) ? $data[$name] : $default;
-  }
-
-  /**
    * Set a setting value
    *
-   * @param string $group The group name to put the value on 
+   * @param string $group The group name to put the value on
    * @param string $name  Either the name of the config inside the group
    *                      or the value if the setting is not in a group
    * @param string $value If the setting is inside a group, this is the value
@@ -114,6 +63,48 @@ class sfSympalConfig extends sfConfig
     {
       self::$config['app_sympal_config_'.$group][$name] = $value;
     }
+  }
+
+  /**
+   * Helper to return very deep configuration values
+   *
+   * all:
+   *   sympal_config:
+   *     group:
+   *       subGroup:
+   *         name:      value
+   *
+   * @param string $group The group name for the config
+   * @param string $subGroup The sub group name for the config
+   * @param string $name The name of the config key
+   * @param mixed $default The default value to return if the config doesn't exist
+   */
+  public static function getDeep($group, $subgroup, $name, $default = null)
+  {
+    $data = sfSympalConfig::get($group, $subgroup);
+
+    if (!$data || !is_array($data))
+    {
+      return $default;
+    }
+
+    return isset($data[$name]) ? $data[$name] : $default;
+  }
+
+  /**
+   * Returns the "name" of the current site
+   *
+   * @throws sfException
+   * @return string
+   */
+  public static function getCurrentSiteName()
+  {
+    if (!$site = sfConfig::get('sf_sympal_site', sfConfig::get('sf_app')))
+    {
+      throw new sfException('No current site defined. If you receive this error in a task, be sure to initialize an application configuration.');
+    }
+
+    return $site;
   }
 
   /**
@@ -168,41 +159,6 @@ class sfSympalConfig extends sfConfig
   }
 
   /**
-   * Get a overridden asset path or return the original asset path
-   *
-   * @param string $path 
-   * @return string $path
-   */
-  public static function getAssetPath($path)
-  {
-    return isset(self::$config['app_sympal_config_asset_paths'][$path]) ? self::$config['app_sympal_config_asset_paths'][$path] : $path;
-  }
-
-  /**
-   * Get name of the admin generator theme to use
-   *
-   * @return string $theme
-   */
-  public static function getAdminGeneratorTheme()
-  {
-    $theme = sfSympalConfig::get('themes', sfSympalConfig::get('admin_theme'));
-
-    return isset($theme['admin_generator_theme']) ? $theme['admin_generator_theme'] : sfSympalConfig::get('default_admin_generator_theme', null, 'sympal_admin');
-  }
-
-  /**
-   * Get the name of the admin generator class to use
-   *
-   * @return string $class
-   */
-  public static function getAdminGeneratorClass()
-  {
-    $theme = sfSympalConfig::get('themes', sfSympalConfig::get('admin_theme'));
-
-    return isset($theme['admin_generator_class']) ? $theme['admin_generator_class'] : sfSympalConfig::get('default_admin_generator_class', null, 'sfSympalDoctrineGenerator');
-  }
-
-  /**
    * Get array of configured content templates for a given model name
    *
    * @param string $model
@@ -251,5 +207,62 @@ class sfSympalConfig extends sfConfig
 
     sfSympalConfig::set($group, $name, $value);
     file_put_contents($path, sfYaml::dump($array, 4));
+  }
+
+  /**
+   * Get a overridden asset path or return the original asset path
+   *
+   * @param string $path
+   * @return string $path
+   */
+  public static function getAssetPath($path)
+  {
+    return isset(self::$config['app_sympal_config_asset_paths'][$path]) ? self::$config['app_sympal_config_asset_paths'][$path] : $path;
+  }
+
+  /**
+   * Get name of the admin generator theme to use
+   *
+   * @return string $theme
+   */
+  public static function getAdminGeneratorTheme()
+  {
+    $theme = sfSympalConfig::get('themes', sfSympalConfig::get('admin_theme'));
+
+    return isset($theme['admin_generator_theme']) ? $theme['admin_generator_theme'] : sfSympalConfig::get('default_admin_generator_theme', null, 'sympal_admin');
+  }
+
+  /**
+   * Get the name of the admin generator class to use
+   *
+   * @return string $class
+   */
+  public static function getAdminGeneratorClass()
+  {
+    $theme = sfSympalConfig::get('themes', sfSympalConfig::get('admin_theme'));
+
+    return isset($theme['admin_generator_class']) ? $theme['admin_generator_class'] : sfSympalConfig::get('default_admin_generator_class', null, 'sfSympalDoctrineGenerator');
+  }
+
+  /**
+   * Check whether a Doctrine query result cache key should use result cache or not
+   *
+   * @param string $key
+   * @return boolean
+   */
+  public static function shouldUseResultCache($key)
+  {
+    if (isset(self::$config['app_sympal_config_orm_cache']['queries'][$key]['enabled'])
+      && self::$config['app_sympal_config_orm_cache']['queries'][$key]['enabled']
+      && isset(self::$config['app_sympal_config_orm_cache']['result'])
+      && self::$config['app_sympal_config_orm_cache']['result']
+    )
+    {
+      return isset(self::$config['app_sympal_config_orm_cache']['queries'][$key]['lifetime']) ? self::$config['app_sympal_config_orm_cache']['queries'][$key]['lifetime'] : self::$config['app_sympal_config_orm_cache']['lifetime'];
+    }
+    else
+    {
+      return false;
+    }
   }
 }
