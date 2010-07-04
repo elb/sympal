bd8f68b18f4fae85f25961556a5f12b1aa2ca6ff
Ryan Weaver <ryan@thatsquality.com>
2010-07-04 12:44:50 -0500

    [test] Adding unit test for sfSympalSiteTable and added some php doc to the corresponding class.

---
+++ test/unit/model/doctrine/PluginsfSympalSiteTableTest.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once(dirname(__FILE__).'/../../../bootstrap/functional.php');
+
+$t = new lime_test(5);
+$tbl = Doctrine_Core::getTable('sfSympalSite');
+
+$t->info('1 - Test fetchCurrent().');
+  $site = $tbl->fetchCurrent();
+  $t->is($site, null, '->fetchCurrent() does not automatically create the record for you.');
+
+  $site = $tbl->fetchCurrent(true);
+  $t->is(get_class($site), 'sfSympalSite', '->fetchCurrnet(true) will create the record');
+  $t->is($site->name, 'sympal', 'The new site record is given the right name.');
+
+  $site = $tbl->fetchCurrent(true);
+  $t->is(get_class($site), 'sfSympalSite', '->fetchCurrnet() now returns the existent record');
+  $siteCount = $tbl->createQuery()->count();
+  $t->is($siteCount, 1, 'Only 1 site record was created throughout the process.');
