48a2bac328544bbfef3fa406d79d7f6696a0cdd4
Ryan Weaver <ryan@thatsquality.com>
2010-07-04 10:59:58 -0500

    [tes] Slightly cleaning up an old unit test

--- test/unit/model/doctrine/PluginsfSympalSiteTest.php
+++ test/unit/model/doctrine/PluginsfSympalSiteTest.php
@@ -1,23 +1,24 @@
 <?php
 
 $app = 'sympal';
-require_once(dirname(__FILE__).'/../../../bootstrap/unit.php');
+require_once(dirname(__FILE__).'/../../../bootstrap/functional.php');
+require_once $configuration->getSymfonyLibDir().'/vendor/lime/lime.php';
 
 $t = new lime_test(5);
 
 $t->info('1 - Test that the delete recurses on the application level to the sfSympalContent records');
 $site = Doctrine_Core::getTable('sfSympalSite')->findOneBySlug('sympal');
-$pages = Doctrine_Core::getTable('sfSympalPage')->createQuery()->execute();
+$products = Doctrine_Core::getTable('Product')->createQuery()->execute();
 
 $t->isnt(count($site->Content), 0, 'Sanity check: the sfSympalSite record has at least one Content record');
-$t->isnt(count($pages), 0, 'Sanity check: We begin with more than 0 sfSympalPage objects');
+$t->isnt(count($products), 0, 'Sanity check: We begin with more than 0 Product objects');
 
 $site->delete();
 
 $site->refreshRelated('Content');
-$pages = Doctrine_Core::getTable('sfSympalPage')->createQuery()->execute();
+$products = Doctrine_Core::getTable('Product')->createQuery()->execute();
 $t->is(count($site->Content), 0, 'The site now has no content. This would happen with or without the application-level delete recursion');
-$t->is(count($pages), 0, 'All of the sfSympalPage records are gone due to the application-level delete recursion onto sfSympalContent');
+$t->is(count($products), 0, 'All of the Product records are gone due to the application-level delete recursion onto sfSympalContent');
 
 $t->info('2 - Quick check on ->deleteApplication');
 $site->deleteApplication();
