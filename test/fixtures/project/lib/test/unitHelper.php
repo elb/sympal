5a57cd0254cf5ef3bda381097e54cb4856018830
weaverryan <ryan@thatsquality.com>
2010-07-09 17:22:52 -0500

    Cleaning up old stuff in unit helper

--- test/fixtures/project/lib/test/unitHelper.php
+++ test/fixtures/project/lib/test/unitHelper.php
@@ -1,27 +1,5 @@
 <?php
 
-// creates a content type record
-function create_content_type(lime_test $t, $name)
-{
-  $t->info('...create a content type for '.$name);
-  $type = new sfSympalContentType();
-  $type->name = $name;
-  $type->label = $name;
-  $type->save();
-
-  return $type;
-}
-
-// creates a content record of a given type
-function create_content(lime_test $t, $type)
-{
-  $t->info('...create a Content record linking to '.$type);
-  $content = sfSympalContent::createNew($type);
-  $content->save();
-
-  return $content;
-}
-
 // attaches and returns a new Doctrine_Connection_Profiler
 function create_doctrine_profiler(Doctrine_Connection $conn = null)
 {
