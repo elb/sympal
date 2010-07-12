9e16dfc34328dce0800de35acb72fa9d496b9f62
Ryan Weaver <ryan@thatsquality.com>
2010-07-12 08:40:09 -0500

    Adding Page to the listener test.

--- test/unit/behaviors/sfSympalContentTypeListenerTest.php
+++ test/unit/behaviors/sfSympalContentTypeListenerTest.php
@@ -4,7 +4,7 @@ $app = 'sympal';
 require_once(dirname(__FILE__).'/../../bootstrap/functional.php');
 require_once $configuration->getSymfonyLibDir().'/vendor/lime/lime.php';
 
-$t = new lime_test(6);
+$t = new lime_test(11);
 $tbl = Doctrine_Core::getTable('Product');
 
 $t->info('...create a Product record.');
@@ -26,4 +26,16 @@ $t->info('1 - Test the ->Content and ->Content->ContentType relations on the new
   $t->is($content->content_type_id, $type->id, 'The $content->Type relationship was saved correctly.');
   $t->is($type->default_path, '/product/:slug', '$type->default_path was saved as /product/:slug because of the slug field');
   $t->is($type->type_key, 'product', '$type->type_key was saved correctly.');
+
+$t->info('2 - Run a similar test on the Page model');
+
+  $page = new Page();
+  $page->save();
+  $page->refresh(true);
+
+  $t->is(get_class($page), 'Page', 'The page persisted as normal');
+  $t->isnt($page->Content->id, null, 'The ->Content relationship was saved correctly.');
+  $t->isnt($page->Content->Type->id, null, 'The $content->Type relationship was saved correctly.');
+  $t->is($page->Content->Type->default_path, '/page/:id', '$type->default_path was saved as /page/:id because no slug field');
+  $t->is($page->Content->Type->type_key, 'page', '$type->type_key was saved correctly.');
   
