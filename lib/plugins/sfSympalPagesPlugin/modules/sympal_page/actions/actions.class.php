c76016e7f96d40aaf7503fc5dcf76f80735486f9
Ryan Weaver <ryan@thatsquality.com>
2010-07-23 07:07:07 -0500

    Adding the beginning of a sfSympalPage admin module

---
+++ lib/plugins/sfSympalPagesPlugin/modules/sympal_page/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/sympal_pageGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/sympal_pageGeneratorHelper.class.php';
+
+/**
+ * sf_sympal_page actions.
+ *
+ * @package    uhp
+ * @subpackage sf_sympal_page
+ * @author     Ryan Weaver <ryan@thatsquality.com>
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sympal_pageActions extends autoSympal_pageActions
+{
+}
---
+++ lib/plugins/sfSympalPagesPlugin/modules/sympal_page/lib/sympal_pageGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * sf_sympal_page module helper.
+ *
+ * @package    uhp
+ * @subpackage sf_sympal_page
+ * @author     Ryan Weaver <ryan@thatsquality.com>
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sympal_pageGeneratorHelper extends BaseSympal_pageGeneratorHelper
+{
+}
